a2ed9972b5416dc989002ce76e41e6a2f8366c3d
MrFasolo97 <79fasolo97@gmail.com>
2025-04-21 15:30:03 +0000

    fix: rsa on new library versions

--- src/crypto/rsa.ts
+++ src/crypto/rsa.ts
@@ -1,130 +1,108 @@
-// @ts-ignore
-import * as pki from 'node-forge/lib/pki'
-// @ts-ignore
-import * as md from 'node-forge/lib/md'
-// @ts-ignore
-import * as md5 from 'node-forge/lib/md5'
-// @ts-ignore
-import * as util from 'node-forge/lib/util'
+import { createPrivateKey, createPublicKey, privateEncrypt, publicDecrypt } from 'crypto'
+import {
+  sha3_224, sha3_256, sha3_384, sha3_512
+} from 'js-sha3'
 import { RSADigestAlgorithm, TBytes, TRSAKeyPair } from './interface'
 import { base64Decode, base64Encode } from '../conversions/base-xx'
 import { stringToBytes, bytesToString } from '../conversions/string-bytes'
-import * as sha3 from 'js-sha3'
+import { createSign, createVerify, generateKeyPairSync, generateKeyPair, constants } from 'crypto'
 
-// HACK. Monkey patch node-forge library to provide oids for missing hash algorithms
-pki.oids['sha224'] = '2.16.840.1.101.3.4.2.4'
-pki.oids['2.16.840.1.101.3.4.2.4'] = 'sha224'
-
-pki.oids['sha3-224'] = '2.16.840.1.101.3.4.2.7'
-pki.oids['2.16.840.1.101.3.4.2.7'] = 'sha3-224'
-
-pki.oids['sha3-256'] = '2.16.840.1.101.3.4.2.8'
-pki.oids['2.16.840.1.101.3.4.2.8'] = 'sha3-256'
-
-pki.oids['sha3-384'] = '2.16.840.1.101.3.4.2.9'
-pki.oids['2.16.840.1.101.3.4.2.9'] = 'sha3-384'
-
-pki.oids['sha3-512'] = '2.16.840.1.101.3.4.2.10'
-pki.oids['2.16.840.1.101.3.4.2.10'] = 'sha3-512'
-
-
-export const pemToBytes = (pem: string) => base64Decode(
-  pem.trim()
-    .split(/\r\n|\n/)
-    .slice(1, -1).join('')
-    .trim()
-)
+export const pemToBytes = (pem: string): Uint8Array =>
+  base64Decode(pem.trim().split(/\r?\n/).slice(1, -1).join(''));
 
 const pemTypeMap = {
   rsaPrivateNonEncrypted: 'RSA PRIVATE KEY',
   rsaPublic: 'PUBLIC KEY',
-}
-export const bytesToPem = (bytes: Uint8Array, type: keyof typeof pemTypeMap) => {
-  const header = `-----BEGIN ${pemTypeMap[type]}-----\n`
-  const footer = `-----END ${pemTypeMap[type]}-----\n`
+};
 
-  let b64characters = base64Encode(bytes)
-  if (b64characters.length % 64 !== 0) {
-    b64characters += ' '.repeat(64 - b64characters.length % 64)
-  }
+export const bytesToPem = (bytes: Uint8Array, type: keyof typeof pemTypeMap): string => {
+  const header = `-----BEGIN ${pemTypeMap[type]}-----\n`;
+  const footer = `-----END ${pemTypeMap[type]}-----\n`;
 
-  let result = header
-  for (let i = 0; i < (b64characters.length / 64); i++) {
-    result += b64characters.slice(i * 64, (i + 1) * 64) + '\n'
-  }
-  result += footer
+  const base64 = base64Encode(bytes).replace(/(.{64})/g, '$1\n');
+  return `${header}${base64}\n${footer}`;
+};
 
-  return result
-}
-
-export const rsaKeyPairSync = (bits?: number, e?: number): TRSAKeyPair => {
-  const kp = pki.rsa.generateKeyPair(bits, e)
-  return {
-    rsaPrivate: pemToBytes(pki.privateKeyToPem(kp.privateKey)),
-    rsaPublic: pemToBytes(pki.publicKeyToPem(kp.publicKey)),
-  }
+function derToPem(der: Uint8Array, type: 'pkcs1' | 'pkcs8' | 'spki'): string {
+  const b64 = Buffer.from(der).toString('base64')
+  const lines = b64.match(/.{1,64}/g)?.join('\n')
+  const header =
+    type === 'pkcs1' ? 'RSA PRIVATE KEY' :
+    type === 'spki'  ? 'PUBLIC KEY' :
+                       'PRIVATE KEY'
+  return `-----BEGIN ${header}-----\n${lines}\n-----END ${header}-----`
 }
 
-export const rsaKeyPair = async (bits?: number, e?: number): Promise<TRSAKeyPair> =>
-  new Promise<TRSAKeyPair>((resolve, reject) => {
-    pki.rsa.generateKeyPair(bits, e, function (err: any, kp: any) {
-      if (err) reject(err)
+export function rsaSign(
+  privateKeyBytes: Uint8Array,
+  message: Uint8Array,
+  digest: 'SHA256' | 'SHA1' | 'MD5' = 'SHA256'
+): Uint8Array {
+  const pem = derToPem(privateKeyBytes, 'pkcs1')
+  const privateKey = createPrivateKey({ key: pem, format: 'pem', type: 'pkcs1' })
+  const signer = createSign(digest)
+  signer.update(message)
+  signer.end()
+  return signer.sign(privateKey)
+}
+
+// Digest function map
+const digestMap: Record<RSADigestAlgorithm, (msg: Uint8Array) => Uint8Array> = {
+  'SHA3-224': msg => Uint8Array.from(sha3_224.array(msg)),
+  'SHA3-256': msg => Uint8Array.from(sha3_256.array(msg)),
+  'SHA3-384': msg => Uint8Array.from(sha3_384.array(msg)),
+  'SHA3-512': msg => Uint8Array.from(sha3_512.array(msg)),
+  // Other digests not supported by this implementation
+  'SHA256': () => { throw new Error('Use Node.js crypto for SHA256') },
+  'SHA1': () => { throw new Error('Use Node.js crypto for SHA1') },
+  'MD5': () => { throw new Error('Use Node.js crypto for MD5') },
+  'SHA224': () => { throw new Error('Use Node.js crypto for SHA224') },
+  'SHA384': () => { throw new Error('Use Node.js crypto for SHA384') },
+  'SHA512': () => { throw new Error('Use Node.js crypto for SHA512') },
+};
+
+export const rsaKeyPair = async (): Promise<TRSAKeyPair> => {
+  return new Promise((resolve, reject) => {
+    generateKeyPair('rsa', {
+      modulusLength: 2048,
+      publicExponent: 0x10001,
+      publicKeyEncoding: { type: 'spki', format: 'pem' },
+      privateKeyEncoding: { type: 'pkcs1', format: 'pem' }
+    }, (err, publicKey, privateKey) => {
+      if (err) return reject(err)
       resolve({
-        rsaPrivate: pemToBytes(pki.privateKeyToPem(kp.privateKey)),
-        rsaPublic: pemToBytes(pki.publicKeyToPem(kp.publicKey)),
+        rsaPrivate: pemToBytes(privateKey),
+        rsaPublic: pemToBytes(publicKey)
       })
     })
   })
-
-const digestCreatorPlaceHolder: any = (type: string) => () => {
-  throw new Error(`Digest ${type} is unsupported`)
 }
 
-class MessageDigestAdapter implements md.MessageDigest {
-  constructor(private sha3Digest: sha3.Hasher, private algorithm: string){}
+export const rsaKeyPairSync = (): TRSAKeyPair => {
+  const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicExponent: 0x10001,
+    privateKeyEncoding: { type: 'pkcs1', format: 'pem' },
+    publicKeyEncoding: { type: 'spki', format: 'pem' }
+  })
 
-  static makeCreator(sha3Hash: sha3.Hash, algorithmName: string): { create(): md.MessageDigest } {
-    return {create: () => new MessageDigestAdapter(sha3Hash.create(), algorithmName)}
-  }
-
-  public update(msg: string, encoding?: 'raw' | 'utf8'): md.MessageDigest {
-    this.sha3Digest.update(stringToBytes(msg, encoding == null ? 'raw' : encoding))
-    return this
-  }
-
-  public digest(): util.ByteStringBuffer{
-    const bytes = Uint8Array.from(this.sha3Digest.digest())
-    return util.createBuffer(bytesToString(bytes, 'raw'))
+  return {
+    rsaPrivate: pemToBytes(privateKey),
+    rsaPublic: pemToBytes(publicKey),
   }
 }
 
-const digestMap: Record<RSADigestAlgorithm, { create(): md.MessageDigest }> = {
-  'MD5': md5,
-  'SHA1': md.algorithms.sha1,
-  'SHA224': digestCreatorPlaceHolder('SHA224'),
-  'SHA256': md.algorithms.sha256,
-  'SHA384': md.algorithms.sha384,
-  'SHA512': md.algorithms.sha512,
-  'SHA3-224': MessageDigestAdapter.makeCreator(sha3.sha3_224, 'sha3-224'),
-  'SHA3-256': MessageDigestAdapter.makeCreator(sha3.sha3_256, 'sha3-256'),
-  'SHA3-384': MessageDigestAdapter.makeCreator(sha3.sha3_384, 'sha3-384'),
-  'SHA3-512': MessageDigestAdapter.makeCreator(sha3.sha3_512, 'sha3-512'),
+export function rsaVerify(
+  publicKeyBytes: Uint8Array,
+  message: Uint8Array,
+  signature: Uint8Array,
+  digest: 'SHA256' | 'SHA1' | 'MD5' = 'SHA256'
+): boolean {
+  const pem = derToPem(publicKeyBytes, 'spki')
+  const publicKey = createPublicKey({ key: pem, format: 'pem', type: 'spki' })
+  const verifier = createVerify(digest)
+  verifier.update(message)
+  verifier.end()
+  return verifier.verify(publicKey, signature)
 }
-
-export const rsaSign = (rsaPrivateKey: TBytes, message: TBytes, digest: RSADigestAlgorithm = 'SHA256'): TBytes => {
-  const s = bytesToPem(rsaPrivateKey, 'rsaPrivateNonEncrypted')
-  const sk = pki.privateKeyFromPem(s) as pki.rsa.PrivateKey
-  const _digest = digestMap[digest].create()
-  _digest.update(bytesToString(message, 'raw'))
-  return stringToBytes(sk.sign(_digest), 'raw')
-}
-
-export const rsaVerify = (rsaPublicKey: TBytes, message: TBytes, signature: TBytes, digest: RSADigestAlgorithm = 'SHA256'): boolean => {
-  const pk = pki.publicKeyFromPem(bytesToPem(rsaPublicKey, 'rsaPublic')) as pki.rsa.PublicKey
-  const _digest = digestMap[digest].create()
-  _digest.update(bytesToString(message), 'raw')
-  return pk.verify(_digest.digest().getBytes(), bytesToString(signature, 'raw'))
-}
-
-
 
